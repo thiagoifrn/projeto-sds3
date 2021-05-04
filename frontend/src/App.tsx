4b5720383740909770d9da2ce4671fa0c1c09ee5
thiagoifrn <thiagoifrn@gmail.com>
2021-05-04 18:44:07 -0300

    Dashboard para Painel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
 
       <div className="container">
-        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+        <h1 className="text-primary py-3">Painel de Vendas</h1>
         <div className="row px-3"> 
           <div className="col-sm-6">
               <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
